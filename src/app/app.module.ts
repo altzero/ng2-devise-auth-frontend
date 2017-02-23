958067458a8f6cb4382a30a53a73f50899a0d3ba
Adam Lutz <amlutz160@gmail.com>
2017-02-22 22:31:36 -0600

    adding ng2 tokenService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { Angular2TokenService } from 'angular2-token';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        HttpModule,
+        RouterModule
+    ],
+    declarations: [ AppComponent ],
+    providers:    [ Angular2TokenService ],
+    bootstrap:    [ AppComponent ]
 })
+
 export class AppModule { }
